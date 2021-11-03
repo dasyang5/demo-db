d9ce2b9071ac78a9184dd5d0be6500be706b216f
dasyang5 <37821855+dasyang5@users.noreply.github.com>
2021-11-03 09:37:31 +0800

    fix(放行/user/add):

--- src/main/java/com/example/demo/config/security/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/config/security/SpringSecurityConfig.java
@@ -23,6 +23,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/user/add").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
